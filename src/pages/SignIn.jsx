961b1382a0b652c8ce2f618145d8d7ab1a27abd5
giuzep89 <g.rinaldimusic@gmail.com>
2025-11-17 15:43:46 +0100

    Bonus 1 added

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,22 +1,36 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import { Link } from 'react-router-dom';
 import {AuthContext} from "../components/authcontext/AuthContext";
 
 function SignIn() {
     const {login} = useContext(AuthContext);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    });
+
+    function handleFormChange(e, fieldText){
+        setFormData({...formData, [fieldText]: e.target.value});
+    }
+
+    function handleFormSubmit(){
+        login();
+        console.log(formData);
+    }
 
   return (
     <>
-        {/*{console.log(data)}*/}
       <h1>Inloggen</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
-
-      <form>
-        <p>*invoervelden*</p>
-        <button
-        type="button" onClick={() => login()}>Inloggen</button>
+      <form onSubmit={handleFormSubmit}>
+          <label>Email
+              <input type="text" id="email" onChange={(e) => handleFormChange(e, "email")}/>
+          </label>
+          <label>Password
+              <input type="text" id="password" onChange={(e) => handleFormChange(e, "password")}/>
+          </label>
+        <button type="submit">Inloggen</button>
       </form>
-
       <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
     </>
   );
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 function SignUp() {
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: ""
+    });
+
+    function handleFormChange(e, fieldText){
+        setFormData({...formData, [fieldText]: e.target.value});
+    }
+
+    function handleFormSubmit(){
+        console.log(formData);
+    }
+
   return (
     <>
       <h1>Registreren</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur atque consectetur, dolore eaque eligendi
         harum, numquam, placeat quisquam repellat rerum suscipit ullam vitae. A ab ad assumenda, consequuntur deserunt
         doloremque ea eveniet facere fuga illum in numquam quia reiciendis rem sequi tenetur veniam?</p>
-      <form>
-        <p>*Invoervelden*</p>
-      </form>
+          <form onSubmit={handleFormSubmit}>
+              <label>Email
+                  <input type="text" id="email" onChange={(e) => handleFormChange(e, "username")}/>
+              </label>
+              <label>Password
+                  <input type="text" id="password" onChange={(e) => handleFormChange(e, "password")}/>
+              </label>
+              <label>Username
+                  <input type="text" id="username" onChange={(e) => handleFormChange(e, "password")}/>
+              </label>
+              <button type="submit">Inloggen</button>
+          </form>
       <p>Heb je al een account? Je kunt je <Link to="/signin">hier</Link> inloggen.</p>
     </>
   );
