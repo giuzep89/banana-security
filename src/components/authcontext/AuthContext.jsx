9c3520efb9d33bc4ee34c25af5ea8183e432b566
giuzep89 <g.rinaldimusic@gmail.com>
2025-11-17 14:25:25 +0100

    All steps of the basic stappenplan completed

--- src/components/authcontext/AuthContext.jsx
+++ src/components/authcontext/AuthContext.jsx
@@ -1,16 +1,22 @@
 import {createContext, useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 export const AuthContext = createContext({});
 
 function AuthContextProvider({children}) {
+    const navigate = useNavigate();
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
     function login(){
         setIsAuthenticated(true);
+        console.log("Gebruiker is ingelogd!");
+        navigate("/profile");
     }
 
     function logout(){
         setIsAuthenticated(false);
+        console.log("Gebruiker is uitgelogd!");
+        navigate("/");
     }
 
     const data = {
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
+import {AuthContext} from "../components/authcontext/AuthContext";
 
 function SignIn() {
+    const {login} = useContext(AuthContext);
+
   return (
     <>
+        {/*{console.log(data)}*/}
       <h1>Inloggen</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab alias cum debitis dolor dolore fuga id molestias qui quo unde?</p>
 
       <form>
         <p>*invoervelden*</p>
-        <button>Inloggen</button>
+        <button
+        type="button" onClick={() => login()}>Inloggen</button>
       </form>
 
       <p>Heb je nog geen account? <Link to="/signup">Registreer</Link> je dan eerst.</p>
